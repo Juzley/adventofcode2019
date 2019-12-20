a28841900e6340caeb935cc00da8265ad719ef27
Julian Priestley <jupriest@fb.com>
2019-12-20 00:04:18 +0000

    Day 18 Complete

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -10,17 +10,17 @@ type Coords = (usize, usize);
 enum Tile {
     Wall,
     Floor,
-    Entrance,
+    Entrance(Coords),
     Key(char),
     Door(char),
 }
 
 impl Tile {
-    fn from_char(c: char) -> Self {
+    fn from_char(c: char, coords: Coords) -> Self {
         match c {
             '#' => Tile::Wall,
             '.' => Tile::Floor,
-            '@' => Tile::Entrance,
+            '@' => Tile::Entrance(coords),
             'a'..='z' => Tile::Key(c),
             'A'..='Z' => Tile::Door(c.to_lowercase().next().unwrap()),
             _ => panic!("Unexpected tile char"),
@@ -31,7 +31,7 @@ impl Tile {
 #[derive(Debug)]
 struct Map {
     tiles: Vec<Vec<Tile>>,
-    start: Coords,
+    starts: Vec<Tile>,
     keys: HashMap<char, Coords>,
 
     // Map from a tile (the entrance or a key) to a vector containing
@@ -42,29 +42,29 @@ struct Map {
 
 impl Map {
     fn from_lines(lines: &[String]) -> Self {
-        let tiles: Vec<Vec<Tile>> = lines
-            .iter()
-            .map(|l| l.chars().map(|c| Tile::from_char(c)).collect::<Vec<Tile>>())
-            .collect();
-
-        let mut start = None;
+        let mut tiles = Vec::new();
+        let mut starts = Vec::new();
         let mut keys = HashMap::new();
-        for (y, row) in tiles.iter().enumerate() {
-            for (x, tile) in row.iter().enumerate() {
+        for (y, line) in lines.iter().enumerate() {
+            let mut row = Vec::new();
+            for (x, c) in line.chars().enumerate() {
                 let coords = (x, y);
+                let tile = Tile::from_char(c, coords);
+                row.push(tile);
                 match tile {
-                    Tile::Entrance => start = Some(coords),
+                    Tile::Entrance(_) => starts.push(tile),
                     Tile::Key(c) => {
-                        let _ = keys.insert(*c, coords);
+                        let _ = keys.insert(c, coords);
                     }
                     _ => (),
                 }
             }
+            tiles.push(row);
         }
 
         Map {
             tiles: tiles,
-            start: start.unwrap(),
+            starts: starts,
             keys: keys,
             reachability: HashMap::new(),
         }
@@ -127,7 +127,7 @@ impl Map {
                         new_required_keys.insert(c);
                         queue.push_back((coords, distance, new_required_keys));
                     }
-                    Tile::Floor | Tile::Entrance => {
+                    Tile::Floor | Tile::Entrance(_) => {
                         queue.push_back((coords, distance, required_keys.clone()))
                     }
 
@@ -140,54 +140,87 @@ impl Map {
     }
 
     fn build_reachability(&mut self) {
-        let start_info = self.find_keys_from_coords(self.start);
+        let start_info: HashMap<Tile, _> = self
+            .starts
+            .iter()
+            .map(|tile| match tile {
+                Tile::Entrance(coords) => (*tile, self.find_keys_from_coords(*coords)),
+                _ => panic!("Wrong start tile type"),
+            })
+            .collect();
         let key_info: HashMap<Tile, _> = self
             .keys
             .iter()
             .map(|(k, coords)| (Tile::Key(*k), self.find_keys_from_coords(*coords)))
             .collect();
 
-        self.reachability.insert(Tile::Entrance, start_info);
+        self.reachability.extend(start_info);
         self.reachability.extend(key_info);
     }
 
-    fn make_memo_key(key: char, keys: &HashSet<char>) -> String {
+    fn make_memo_key(current_locs: &Vec<Tile>, keys: &HashSet<char>) -> String {
+        // Don't sort the locations - the order is important for the case where more
+        // than one current location is at an entrance
+        let loc_str: String = current_locs
+            .iter()
+            .map(|t| match t {
+                Tile::Entrance(_) => '@',
+                Tile::Key(c) => *c,
+                _ => panic!("Current location neither an entrance nor a key"),
+            })
+            .collect();
+
         let mut keyvec = Vec::new();
         for c in keys {
             keyvec.push(*c);
         }
         keyvec.sort();
 
-        format!("{}{}", key.to_string(), keyvec.iter().collect::<String>())
+        format!("{}{}", loc_str, keyvec.iter().collect::<String>())
     }
 
     fn find_shortest_path(
         &self,
         keys: HashSet<char>,
-        tile: Tile,
+        current_tiles: Vec<Tile>,
         memo: &mut HashMap<String, usize>,
     ) -> usize {
-        let reachability: Vec<usize> = self.reachability[&tile]
-            .iter()
-            .filter(|(c, _, req_keys)| !keys.contains(c) && req_keys.is_subset(&keys))
-            .map(|(c, d, _)| {
-                let memo_key = Map::make_memo_key(*c, &keys);
-                if let Some(distance) = memo.get(&memo_key) {
-                    d + *distance
-                } else {
-                    let mut keys = keys.clone();
-                    keys.insert(*c);
-                    let distance = self.find_shortest_path(keys, Tile::Key(*c), memo);
-                    let _ = memo.insert(memo_key, distance);
-                    d + distance
-                }
-            })
-            .collect();
+        if keys.len() == self.keys.len() {
+            return 0;
+        }
 
-        if reachability.is_empty() {
+        let mut all_distances = Vec::new();
+        for i in 0..current_tiles.len() {
+            let distances: Vec<usize> = self.reachability[&current_tiles[i]]
+                .iter()
+                .filter(|(c, _, req_keys)| !keys.contains(c) && req_keys.is_subset(&keys))
+                .map(|(c, d, _)| {
+                    let mut new_current_tiles = current_tiles.clone();
+                    new_current_tiles[i] = Tile::Key(*c);
+
+                    let memo_key = Map::make_memo_key(&new_current_tiles, &keys);
+                    if let Some(distance) = memo.get(&memo_key) {
+                        d + *distance
+                    } else {
+                        let mut new_keys = keys.clone();
+                        new_keys.insert(*c);
+
+                        let distance = self.find_shortest_path(new_keys, new_current_tiles, memo);
+                        memo.insert(memo_key, distance);
+                        d + distance
+                    }
+                })
+                .collect();
+
+            if !distances.is_empty() {
+                all_distances.push(*distances.iter().min().unwrap());
+            }
+        }
+
+        if all_distances.is_empty() {
             return 0;
         } else {
-            return *reachability.iter().min().unwrap();
+            return *all_distances.iter().min().unwrap();
         }
     }
 }
@@ -195,8 +228,11 @@ impl Map {
 fn main() {
     let mut map = Map::from_file("input");
     map.build_reachability();
-    println!("{:?}", map);
+    let shortest = map.find_shortest_path(HashSet::new(), map.starts.clone(), &mut HashMap::new());
+    println!("Part 1: {}", shortest);
 
-    let shortest = map.find_shortest_path(HashSet::new(), Tile::Entrance, &mut HashMap::new());
-    println!("{}", shortest);
+    let mut map = Map::from_file("input2");
+    map.build_reachability();
+    let shortest = map.find_shortest_path(HashSet::new(), map.starts.clone(), &mut HashMap::new());
+    println!("Part 2: {}", shortest);
 }
